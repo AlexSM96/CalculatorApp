a1d47bbadd30f342a343cbaa271ab43bc70cf0f4
Alexander <salexm74@gmail.com>
2024-11-24 17:38:24 +0500

    Добавлена простая реализация метода Compute, которая позоваляет решать простые выражени, пример : 1 + 2.

--- CalculatorApp.Source/Calculator.cs
+++ CalculatorApp.Source/Calculator.cs
@@ -1,14 +1,47 @@
-﻿namespace CalculatorApp.Source;
+﻿using System.Text.RegularExpressions;
+
+namespace CalculatorApp.Source;
 
 public class Calculator
 {
+    private readonly Dictionary<string, Func<double, double, double>> _operations;
+
     public Calculator()
     {
-        
+        _operations = new()
+        {
+            ["+"] = (x, y) => x + y,
+            ["-"] = (x, y) => x - y,
+            ["*"] = (x, y) => x * y,
+            ["/"] = (x, y) => x / y,
+        };
     }
 
     public double Compute(string expression)
     {
-        return 0;
+        var tokenizeExpression = Tokenize(expression);
+
+        if(tokenizeExpression.Count != 3 || !double.TryParse(tokenizeExpression[0], out double leftOperand) 
+            || !double.TryParse(tokenizeExpression[2], out double rightOperand))
+        {
+            throw new InvalidOperationException("Неверный формат выражения");
+        }
+
+        if (!_operations.ContainsKey(tokenizeExpression[1]))
+        {
+            throw new InvalidOperationException("Неверный формат выражения");
+        }
+
+        return _operations[tokenizeExpression[1]].Invoke(leftOperand, rightOperand);
+    }
+
+    private List<string> Tokenize(string expression)
+    {
+        var regexPattern = new Regex(@"(\d+\,\d+|\d+|[+*/()-])");
+
+        return regexPattern
+            .Matches(expression)
+            .Select(match => match.Value.ToString())
+            .ToList(); 
     }
 }
